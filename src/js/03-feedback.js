eb1296a1f36be6b4971dbee2bf368fbf0e798e8c
Dzenkinstone <kozlovdanil6@gmail.com>
2022-11-08 12:34:31 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,19 +4,30 @@ console.dir(form);
 
 const STORAGE_KEY = 'feedback-form-state';
 const formData = {};
+let savedString = localStorage.getItem(STORAGE_KEY);
 
 form.addEventListener('input', throttle(onKeyboardPress, 500));
 form.addEventListener('submit', onFormSubmit);
 populateInput();
 
 function onKeyboardPress(event) {
-  formData[event.target.name] = event.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  try {
+    if (savedString) {
+      Object.entries(savedString).forEach(([name, value]) => {
+        formData[name] = value;
+        form.elements[name].value = value;
+      });
+    } else {
+      formData[event.target.name] = event.target.value;
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function populateInput() {
   try {
-    let savedString = localStorage.getItem(STORAGE_KEY);
     savedString = JSON.parse(savedString);
     if (savedString) {
       Object.entries(savedString).forEach(([name, value]) => {
