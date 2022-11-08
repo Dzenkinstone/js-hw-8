9c13c5f490e90e888da464c8ab2825c911803dd9
Dzenkinstone <kozlovdanil6@gmail.com>
2022-11-09 00:10:57 +0100

    /changes/

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,32 +1,46 @@
 import throttle from 'lodash.throttle';
 const form = document.querySelector('form');
-let formData = {};
 const KEY = 'feedback-form-state';
-console.dir(form);
 
 initForm();
 
 form.addEventListener('submit', event => {
   event.preventDefault();
-  localStorage.removeItem(KEY);
-  form.elements.email.value = '';
-  form.elements.message.value = '';
+  try {
+    let persistedData = localStorage.getItem(KEY);
+    persistedData = persistedData ? JSON.parse(persistedData) : {};
+    console.log(persistedData);
+    localStorage.removeItem(KEY);
+  } catch (error) {
+    console.log(error);
+  }
+  event.target.reset();
 });
 
-form.addEventListener('input', event => {
-  let persistedData = localStorage.getItem(KEY);
-  persistedData = persistedData ? JSON.parse(persistedData) : {};
-  persistedData[event.target.name] = event.target.value;
-  localStorage.setItem(KEY, JSON.stringify(persistedData));
-});
+form.addEventListener(
+  'input',
+  throttle(event => {
+    try {
+      let persistedData = localStorage.getItem(KEY);
+      persistedData = persistedData ? JSON.parse(persistedData) : {};
+      persistedData[event.target.name] = event.target.value;
+      localStorage.setItem(KEY, JSON.stringify(persistedData));
+    } catch (error) {
+      console.log(error);
+    }
+  }, 500)
+);
 
 function initForm() {
-  let persistedData = localStorage.getItem(KEY);
-  if (persistedData) {
-    persistedData = JSON.parse(persistedData);
-    console.log(persistedData);
-    Object.entries(persistedData).forEach(([name, value]) => {
-      form.elements[name].value = value;
-    });
+  try {
+    let persistedData = localStorage.getItem(KEY);
+    if (persistedData) {
+      persistedData = JSON.parse(persistedData);
+      Object.entries(persistedData).forEach(([name, value]) => {
+        form.elements[name].value = value;
+      });
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
